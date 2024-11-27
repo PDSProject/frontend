98f02d67bd109d4cdcb432276d175ca3b56c4283
karikapatel29 <karikapatel@gmail.com>
2024-11-27 09:15:38 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './components/Home';
+import FindItem from './components/FindItem';
+import Register from './components/Register';
+import FindOrder from './components/FindOrder';
+import StartOrder from './components/StartOrder';
+import AcceptDonation from './components/AcceptDonation';
+import AllAvailable from './components/AllAvailable';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/finditem" element={<FindItem />} />
+        <Route path="/findorder" element={<FindOrder />} />
+        <Route path="/allavailableitems" element={<AllAvailable />} />
+        <Route path="/startorder" element={<StartOrder />} />
+        <Route path="/acceptdonation" element={<AcceptDonation />} />
+      </Routes>
+    </Router>
   );
 }
 
